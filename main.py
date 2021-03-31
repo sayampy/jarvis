8ffb9a2ae072784cd92a3633e7954f9350becac6
sayampy <81681676+sayampy@users.noreply.github.com>
2021-03-31 21:39:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import Jarvis as jarvis
+
+jarvis.my_name("Sayam")#please replace with your name after fork this
+
+jarvis.my_age(10) #your age
+
+jarvis.info() #for information
+
+jarvis.my_fav("python")
+
+jarvis.intro()
+while True:
+    jarvis.start()
